919274082f78bab27a5e7ac83c8967b3840f6b1e
Renol P. H <reshifr@gmail.com>
2021-11-26 16:24:51 +0700

    style: perbaikan style penulisan

--- lib/prime.c
+++ lib/prime.c
@@ -18,18 +18,16 @@ int is_prime(uint64_t n) {
 uint64_t closest_prime(uint64_t n) {
   uint64_t l, r, nl, nr;
   l = r = nl = nr = 0;
-  for(uint64_t i=n; i>0; --i, ++l) {
+  for(uint64_t i=n; i>0; --i, ++l)
     if( is_prime(i) ) {
       nl = i;
       break;
     }
-  }
-  for(uint64_t i=n;; ++i, ++r) {
+  for(uint64_t i=n;; ++i, ++r)
     if( is_prime(i) ) {
       nr = i;
       break;
     }
-  }
   return r<=l ? nr : nl;
 }
 
